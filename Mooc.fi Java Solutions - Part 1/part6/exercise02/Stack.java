6f38660290008308eb04e8cb4575afe060bedf5c
Onur Atmaca <oatm92@gmail.com>
2022-02-18 17:52:51 -0600

    Add files via upload

---
+++ Mooc.fi Java Solutions - Part 1/part6/exercise02/Stack.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Stack {
+    // public boolean isEmpty() - returns a boolean-type value (true or false) that tells
+    // whether or not the stack is empty.
+    // public void add(String value) - Adds the value given as a parameter to the top of the stack.
+    // public ArrayList<String> values() - returns the values contained in the stack as a list.
+
+    private ArrayList<String> list;
+
+    public Stack() {
+        this.list = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public void add(String value) {
+        list.add(value);
+    }
+
+    public ArrayList<String> values() {
+        return this.list;
+    }
+
+    // Add to the Stack class a public String take() method, which returns the topmost value
+    // (i.e., the last value added to the deque) and removes it from the stack.
+
+    public String take() {
+        return this.list.remove(this.list.size() - 1);
+    }
+
+}
